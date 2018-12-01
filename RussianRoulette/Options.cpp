fb9a5071c460d1cd29d34594b13b947e9dc6d032
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-12-01 18:55:06 -0200

    Added options function

--- RussianRoulette/Options.cpp
+++ RussianRoulette/Options.cpp
@@ -1,5 +1,19 @@
 #include "russianroulette.h"
 
-void options() {
+int CHAMBER_SIZE = 7;
 
+void changeOption(short int inp) {
+	if (inp == 1) {
+		cout << "Enter new chamber size: ";
+		cin >> CHAMBER_SIZE;
+	}
+	else if (inp == 2) { return; }
+	else { cout << "\nInvalid choice\n"; }
+}
+
+void options() {
+	short int choice;
+	cout << "\n===== OPTIONS =====\n[1] Chamber size (" << CHAMBER_SIZE << ")\n[2] Return\n\n> ";
+	cin >> choice;
+	changeOption(choice);
 }
