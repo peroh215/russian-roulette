1429fb7aafe350c09bdb05b353bbecc2d0c767eb
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-12-02 10:46:25 -0200

    Added custom gamemode creation and fixed setgamemode() placement

--- RussianRoulette/Game.cpp
+++ RussianRoulette/Game.cpp
@@ -7,9 +7,19 @@ int PLAYER_AMNT = 2; // Player amount
 int CHAMBER_SIZE = 7; // Gun's chamber size
 std::string gamemode; // For printing current gamemode
 
+int createCustomGamemode() {
+	int player_amount;
+	int chamber_size;
+	cout << "=== Gamemode creation ===\nPlayer amount: ";
+	cin >> player_amount;
+	cout << "Chamber size: ";
+	cin >> chamber_size;
+	return player_amount, chamber_size;
+}
+
 void setGamemode() {
 	short int choice;
-	cout << "\nChoose a gamemode:\n[1] Classic | [2] 50/50\n\n> ";
+	cout << "\nChoose a gamemode:\n[1] Classic | [2] 50/50 | [3] Custom\n\n> ";
 	cin >> choice;
 
 	if (choice == 1) {
@@ -23,6 +33,11 @@ void setGamemode() {
 		gamemode = "50/50";
 		PLAYER_AMNT = 2;
 		CHAMBER_SIZE = 2 - 1; 
+	}
+	else if (choice == 3) {
+		// Custom gamemode
+		gamemode = "Custom";
+		PLAYER_AMNT, CHAMBER_SIZE = createCustomGamemode();
 	}
 	else {
 		cout << "\nInvalid choice\n"; 
@@ -41,11 +56,13 @@ int roll() {
 void game() {
 	bool gameOver = false;
 	int turn = 0;
-	unsigned int bulletLocation = roll();
 	short int playerChoice;
-	cout << "\n=== GAME STARTS ===\nGamemode: " << gamemode << "\nThe chamber is rolled and the gun is given to the player\nThe gun has " << CHAMBER_SIZE << " chambers\n\n";
 
 	setGamemode();
+
+	unsigned int bulletLocation = roll();
+
+	cout << "\n=== GAME STARTS ===\nGamemode: " << gamemode << "\nThe chamber is rolled and the gun is given to the player\nThe gun has " << CHAMBER_SIZE << " chambers\n\n";
 
 	while (gameOver == false) {
 		srand(time(NULL));
