770d07a226b58c7e1b6dfa9f30ca8d6c1efdbe04
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-12-04 16:31:16 -0200

    Added scoreboard base and deleted obsolete code

--- RussianRoulette/russianroulette.h
+++ RussianRoulette/russianroulette.h
@@ -4,9 +4,14 @@
 #include <iostream>
 using namespace std;
 
+// Game
 extern int CHAMBER_SIZE;
 extern int PLAYER_AMNT;
 void game();
-void options();
-#endif
 
+// Scoreboard
+extern int GAMES_WON;
+extern int GAMES_LOST;
+void saveScore();
+void loadScore();
+#endif
