64eab0c5a6faa29027cd58d90e7c8d025a6f5087
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-12-09 00:38:59 -0200

    Added scoreboard related stuff to the menu

--- RussianRoulette/RussianRouletteMenu.cpp
+++ RussianRoulette/RussianRouletteMenu.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 bool running = true;
-static std::string help = "\n[1] Play game\n[2] Help\n[3] Exit";
+static std::string help = "\n[1] Play game\n[2] Save score\n[3] Load scores\n[4] Restart current score\n[5] Restart scoreboard\n[6] Help\n[7] Exit";
 
-void checkInput(int inp) {
-	if (inp == 1) { 
-		game(); // Start game
-		cout << help;
-	}
-	else if (inp == 2) cout << help; // Help
-	else if (inp == 3) exit(0); // Exit
+void checkInput(int inp) {	
+	if (inp == 1) { game(); cout << help;} // Play game / Start game
+	else if (inp == 2) askScore(); // Save score
+	else if (inp == 3) loadScore(); // Load score
+	else if (inp == 4) { ::GAMES_WON = 0; ::GAMES_LOST = 0; cout << "Current score was restarted\n"; } // Restart current RAM score
+	else if (inp == 5) { remove("score.roulette"); cout << "Scoreboard was reseted\n\n"; }
+	else if (inp == 6) cout << help; // Help
+	else if (inp == 7) exit(0); // Exit
 	else cout << "\nInvalid choice\n";
 }
 
